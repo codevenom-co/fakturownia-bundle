3700a3a348cea86e976edd598c4b6c5886a25d46
levan-cdv <levan@codevenom.co>
2026-04-30 12:41:24 +0200

    Mark `readonly` classes, properties, and methods as mutable. Update dependencies in `composer.json` to adapt to Symfony 7.3+. (#3)

--- src/Exception/UnableToRetrieveInvoicesForProvidedPeriodException.php
+++ src/Exception/UnableToRetrieveInvoicesForProvidedPeriodException.php
@@ -6,7 +6,7 @@ use Codevenom\FakturowniaBundle\Invoice\Enum\InvoicePeriod;
 
 class UnableToRetrieveInvoicesForProvidedPeriodException extends FakturowniaException
 {
-    public static function withPeriod(InvoicePeriod $period): self
+    public readonly static function withPeriod(InvoicePeriod $period): self
     {
         return new self(sprintf('Unable to retrieve invoices for provided period: %s', $period->value));
     }
